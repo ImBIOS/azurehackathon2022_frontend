7e95bfd64765714378884217e7c5838e7081adbb
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-06-19 10:39:36 +0700

    enable auth

--- loader.ts
+++
@@ -1,6 +0,0 @@
-//@ts-ignore
-export function imageLoader(params: any) : any {
-  return `/images/${params.src}`; // REPLACE WITH YOUR IMAGE DIRECTORY
-}
-
-export default imageLoader;
---
+++ pages/auth_callback.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+
+const AuthCallbackPage = () => {
+  const router = useRouter();
+  let authToken = "";
+  if (typeof window !== "undefined") {
+    authToken = window.location.hash.replace("#id_token=", "");
+    localStorage.setItem("token", authToken);
+  }
+
+  useEffect(() => {
+    router.push("/input-user");
+  }, [router]);
+
+  return <></>;
+};
+
+export default AuthCallbackPage;
